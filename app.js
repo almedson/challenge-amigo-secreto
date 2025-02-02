eb55f1482426af3441ac557a6265d7872ffe9947
Almedson Ferreira <almedson@gmail.com>
2025-02-02 19:08:27 -0300

    Implementar uma função para adicionar amigos

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ const amigo = document.getElementById('amigo');
 const messsagemErro = document.getElementById('messsagemErro');
 let amigos = [];
 
-
-
 function adicionarAmigo() {
-    // alert("PAssou");
-
     let nomeAmigo = amigo.value.trim() ;
 
     // Validar a entrada: Implemente uma validação para garantir que o campo não esteja vazio.;;
@@ -17,52 +13,30 @@ function adicionarAmigo() {
         messsagemErro.style.display = 'none';
         messsagemErro.innerHTML = '';
         adicionarArrayAmigo(nomeAmigo);
-        amigo.value = '';
+        amigo.value = ''; //Limpar o campo de entrada: Após adicionar o nome, redefina o campo de texto para uma string vazia.
     } else {
         // Se estiver vazio, exiba um alerta com a mensagem de erro: "Por favor, insira um nome."
         messsagemErro.innerHTML = 'Por favor, insira um nome.';
         messsagemErro.style.display = 'block';
         console.log(amigo.value);
     }
-
-    //Atualizar o array de amigos: Se o valor for válido, adicione-o ao array que armazena os nomes 
-    //dos amigos usando o método .push().
-    //amigos.push(amigo);
 }
 
+/*Atualizar o array de amigos: Se o valor for válido, adicione-o ao array que armazena os nomes 
+dos amigos usando o método .push().*/
 function adicionarArrayAmigo(nome){
-    // alert(nome);
     if (nome !== '' && !amigos.includes(nome)) {
         amigos.push(nome);
-        //console.log(`Amigo ${amigo} adicionado à lista.`);
     } else if (amigos.includes(nome)) {
-        // console.log(`Amigo(a) ${nome} já está na lista. Insira outro nome por favor,`);
         messsagemErro.innerHTML = `Amigo(a) ${nome} já está na lista.<br> Insira outro nome por favor,`;
         messsagemErro.style.display = 'block';
     }
-
-// amigos.push('Almedson');
-// amigos.push('Leandro');
 
     for(let i = 0; i < amigos.length; i++){
         console.log(amigos[i]);
     }
 }
 
-
 amigo.addEventListener('change', function() {
     messsagemErro.style.display = 'none';
 });
-
-
-/*
-Capturar o valor do campo de entrada: Utilize document.getElementById ou document.querySelector para obter o texto inserido pelo usuário.
-
-Validar a entrada: Implemente uma validação para garantir que o campo não esteja vazio.
-
-Se estiver vazio, exiba um alerta com a mensagem de erro: "Por favor, insira um nome."
-
-Atualizar o array de amigos: Se o valor for válido, adicione-o ao array que armazena os nomes dos amigos usando o método .push().
-
-Limpar o campo de entrada: Após adicionar o nome, redefina o campo de texto para uma string vazia.
-*/
